b2b05625d3d314457a1aea888c79bd6ae8b0dc35
SooKyungPark <pskthu@gmail.com>
2015-12-09 18:12:01 +0900

    add trim score

--- app/controllers/evaluate_controller.rb
+++ app/controllers/evaluate_controller.rb
@@ -7,18 +7,19 @@ class EvaluateController < ApplicationController
   def edit
     eval_params = params[:evaluate]
     user_id, user_name = ParsingDataSequenceFile.get_user_infos(eval_params[:pdsf_id])
+    score = get_trimmed_score(eval_params[:score].to_i)
   	if eval_params[:pass_or_nonpass] == 'Pass'
-  	  if ParsingDataSequenceFile.insert_eval_infos(eval_params[:pdsf_id], eval_params[:score], true) &&
+  	  if ParsingDataSequenceFile.insert_eval_infos(eval_params[:pdsf_id], score, true) &&
           TaskTable.add_pdsf_items(user_id, user_name, eval_params[:pdsf_id]) &&
-          User.update_score(user_id, eval_params[:score], eval_params[:pdsf_id], true)
+          User.update_score(user_id, score, eval_params[:pdsf_id], true)
   	  	save_done(eval_params[:pdsf_id])
   	  else
   	  	flash[:warning] = "Something's wrong! Please try again."
   	  	redirect_to :action => 'index', :user_id => current_user.user_id
   	  end
   	else
-      if ParsingDataSequenceFile.insert_eval_infos(eval_params[:pdsf_id], eval_params[:score], false) &&
-          User.update_score(user_id, eval_params[:score], eval_params[:pdsf_id], false)
+      if ParsingDataSequenceFile.insert_eval_infos(eval_params[:pdsf_id], score, false) &&
+          User.update_score(user_id, score, eval_params[:pdsf_id], false)
   	    save_done(eval_params[:pdsf_id])
       else
         flash[:warning] = "Something's wrong! Please try again."
@@ -36,5 +37,15 @@ class EvaluateController < ApplicationController
   	    flash[:warning] = "Something's wrong! Please try again."
   	  end
   	  redirect_to :action => 'index', :user_id => current_user.user_id
+    end
+
+    def self.get_trimmed_score(raw_score)
+      if raw_score < 0
+        return 0
+      elsif raw_score > 10
+        return 10
+      else
+        return raw_score
+      end
     end
 end
